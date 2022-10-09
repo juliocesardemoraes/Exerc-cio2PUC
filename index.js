21db193acaa0a02f634302ee656c98ff5e24d9fc
julio cesar <jcmcf2012@gmail.com>
2022-10-09 12:31:42 -0300

    feat - add review section

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function fetchCast(movieId) {
 function fetchReview(movieId) {
   try {
     return fetch(
-      `https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=58a6af7c57bd237d8c8b737a26bdd1ce&language=pt-BR&language=pt-BR&page=1`,
+      `https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=58a6af7c57bd237d8c8b737a26bdd1ce&language=pt-BR&language=en-us&page=1`,
       { mode: "cors" }
     )
       .then(function (response) {
@@ -86,15 +86,156 @@ function fetchMovieVideo(movieId) {
   }
 }
 
+const castRendered = {};
+const reviewsRendered = {};
+
+/**
+ * Function to render all reviews about a singular movie
+ * @returns {DOM Element} Element containing all reviews
+ */
+const renderReviews = () => {
+  const movieId = document.querySelector(".component__show__movie");
+
+  movieIdNumber = movieId.getAttribute("movieId");
+  iteratorNumber = movieId.getAttribute("counterId");
+
+  if (reviewsRendered[movieIdNumber]) {
+    return;
+  }
+  reviewsRendered[movieIdNumber] = true;
+
+  const reviewContainer = document.createElement("div");
+  reviewContainer.setAttribute("id", "review__container");
+
+  const reviewTitle = document.createElement("h2");
+  reviewTitle.classList.add("review__title");
+  reviewTitle.innerHTML = "Avaliações";
+
+  const reviewMainContent = document.createElement("div");
+  reviewMainContent.setAttribute("id", `review${iteratorNumber}`);
+
+  fetchReview(movieIdNumber).then((review) => {
+    for (let i = 0; i < review.results.length; i++) {
+      const reviewFlex = document.createElement("div");
+      reviewFlex.classList.add("flex");
+      reviewFlex.classList.add("review__container");
+      const reviewContentPlaceholder = document.createElement("div");
+      reviewContentPlaceholder.classList.add("profilePictureHolder");
+
+      const imageElement = document.createElement("img");
+      imageElement.classList.add("profilePicture");
+
+      if (review.results[i]?.author_details?.avatar_path) {
+        imageElement.setAttribute(
+          "src",
+          `https://image.tmdb.org/t/p/w500/${review.results[i]?.author_details?.avatar_path}`
+        );
+      } else {
+        imageElement.classList.add("remove__height");
+      }
+
+      reviewContentPlaceholder.append(imageElement);
+      reviewFlex.append(reviewContentPlaceholder);
+
+      const divName = document.createElement("div");
+      const divHeaderName = document.createElement("h3");
+      const spanReview = document.createElement("span");
+
+      divHeaderName.innerHTML =
+        review.results[i]?.author_details?.name || review.results[i]?.author;
+      spanReview.innerHTML = review.results[i]?.content;
+
+      divName.append(divHeaderName);
+      divName.append(spanReview);
+      reviewFlex.append(divName);
+      reviewMainContent.append(reviewFlex);
+    }
+  });
+
+  reviewContainer.append(reviewTitle);
+  reviewContainer.append(reviewMainContent);
+
+  // Had to select again because it has lost its reference
+  const movieIdNew = document.querySelector(".component__show__movie");
+  movieIdNew.append(reviewContainer);
+};
+
+/**
+ * This Function Loads the first user iteration returning all necessary
+ * values about one movie
+ * @returns {DOM Element}
+ */
+const renderFirstCast = () => {
+  const movieId = document.querySelector(".component__show__movie");
+  movieIdNumber = movieId.getAttribute("movieId");
+  iteratorNumber = movieId.getAttribute("id__counter");
+
+  if (castRendered[movieIdNumber]) {
+    return;
+  }
+  castRendered[movieIdNumber] = true;
+
+  const castContainer = document.createElement("div");
+  const castContainerScreenwriter = document.createElement("div");
+  const castContainerActors = document.createElement("div");
+  castContainer.classList.add("movie__info__casting");
+  castContainerScreenwriter.classList.add("movie__info__casting");
+  castContainerActors.classList.add("movie__info__actors");
+
+  fetchCast(movieIdNumber).then((cast) => {
+    const castContainerRole = document.createElement("h3");
+    castContainerRole.innerHTML = "Diretor";
+    castContainer.append(castContainerRole);
+
+    const castContainerRoleScreenplay = document.createElement("h3");
+    castContainerRoleScreenplay.innerHTML = "Roteirista";
+    castContainerScreenwriter.append(castContainerRoleScreenplay);
+
+    const castContainerRoleActors = document.createElement("h3");
+    castContainerRoleActors.innerHTML = "Elenco";
+    castContainerActors.append(castContainerRoleActors);
+
+    const castContainerRoleValue = document.createElement("span");
+    const castContainerRoleValueScreenplay = document.createElement("span");
+
+    const containerActors = document.createElement("div");
+
+    for (let x = 0; x < cast.crew.length; x++) {
+      if (cast.crew[x].job === "Director") {
+        castContainerRoleValue.innerHTML = cast.crew[x].name;
+      }
+      if (cast.crew[x].job === "Screenplay") {
+        castContainerRoleValueScreenplay.innerHTML = cast.crew[x].name;
+      }
+    }
+
+    for (let x = 0; x < cast.cast.length; x++) {
+      if (cast.cast[x].known_for_department === "Acting") {
+        const actorName = document.createElement("span");
+        actorName.innerHTML = cast.cast[x].name;
+        containerActors.append(actorName);
+      }
+    }
+
+    castContainerActors.append(containerActors);
+    castContainer.append(castContainerRoleValue);
+    castContainerScreenwriter.append(castContainerRoleValueScreenplay);
+  });
+
+  movieId.append(castContainer);
+  movieId.append(castContainerScreenwriter);
+  movieId.append(castContainerActors);
+};
+
 /**
  * Move the DOM carousel container
- * @param {string} id
+ * @param {Number} id
  */
 function carouselMove(id) {
   const dotChange = document.getElementById(`dot${id}`);
   const container = document.querySelector("#movie__placeholder");
   const matches = container.querySelectorAll("iframe");
-  for (let i = 0; i < matches.length; ++i) {
+  for (let i = 0; i < matches.length; i++) {
     matches[i].classList.remove("component__show");
     matches[i].classList.add("component__hide");
   }
@@ -106,13 +247,13 @@ function carouselMove(id) {
 
   const carousel = document.querySelector("#carousel");
   const carouselDots = carousel.querySelectorAll("i");
-  for (let i = 0; i < carouselDots.length; ++i) {
+  for (let i = 0; i < carouselDots.length; i++) {
     carouselDots[i].classList.remove("fa-solid");
   }
   dotChange.classList.add("fa-solid");
 
   const allMovieInfo = document.querySelectorAll(".movie__info__container");
-  for (let i = 0; i < allMovieInfo?.length; ++i) {
+  for (let i = 0; i < allMovieInfo?.length; i++) {
     allMovieInfo[i].classList.remove("component__show__movie");
     allMovieInfo[i].classList.add("component__hide__movie");
   }
@@ -129,16 +270,18 @@ function hideOnFirstRender() {
       allMovieInfo[i].classList.remove("component__show__movie");
       allMovieInfo[i].classList.add("component__hide__movie");
     }
+    if (i == 0) {
+      allMovieInfo?.[0].classList.add("component__show__movie");
+    }
   }
-  allMovieInfo[0].classList.add("component__show__movie");
 }
 
 /**
  * Function for rendering movie name and description
  * @param {JSON} movieResult The movies fetched from the api
- * @param {Element} movieInfoHeader The div that the information is going to be inserted
+ * @param {DOM Element} movieInfoHeader The div that the information is going to be inserted
  * @param {Number} i iterator to get the movie
- * @returns {Element} Dom Element
+ * @returns {DOM Element} Dom Element
  */
 function renderMovieNameAndDescription(movieResult, movieInfoHeader, i) {
   const movieInfoNameAndYear = document.createElement("div");
@@ -157,10 +300,10 @@ function renderMovieNameAndDescription(movieResult, movieInfoHeader, i) {
 /**
  * Function for rendering movie review rating and count
  * @param {JSON} movieResult
- * @param {Element} movieInfoHeader
- * @param {Element} movieInfo
+ * @param {DOM Element} movieInfoHeader
+ * @param {DOM Element} movieInfo
  * @param {Number} i
- * @returns {Element} Dom Element
+ * @returns {DOM Element} Dom Element
  */
 function renderMovieRatingAndCount(
   movieResult,
@@ -187,31 +330,6 @@ function renderMovieRatingAndCount(
   movieInfo.append(movieContainer);
 }
 
-function renderMovieCast(movieResult, movieInfo, role, i) {
-  /*
-  const container = document.createElement("div");
-  container.classList.add("movie__info__casting");
-  const containerRole = document.createElement("h3");
-  containerRole.innerHTML = `${role}`;
-  container.append(containerRole);
-  const containerRoleValue = document.createElement("span");
-
-  fetchCast(movieResult?.results[i].id).then(function (movieCast) {
-    console.log(movieCast);
-    for (let x = 0; x < movieCast?.cast?.length; i++) {
-      console.log("movieCast", movieCast?.cast[x]);
-      if (movieCast?.cast[x].know_for_department === "Directing") {
-        containerRoleValue.innerHTML = movieCast?.cast[x].name;
-        break;
-      }
-    }
-  });
-  container.append(containerRoleValue);
-
-  movieInfo.append(container);
-  */
-}
-
 /**
  *
  * @param {string} movieSection the section of the movie
@@ -219,171 +337,98 @@ function renderMovieCast(movieResult, movieInfo, role, i) {
  */
 function loadMovieList(movieSection) {
   document.getElementById("page__loaded").classList.add("component__hide");
-  fetchMovies(movieSection).then(function (movieResult) {
-    if (movieResult) {
-      for (let i = 0; i < movieResult?.results?.length; i++) {
-        fetchMovieVideo(movieResult?.results[i].id)
-          .then(function (movieUrl) {
-            if (movieUrl?.results?.length > 0) {
-              //Render the movie video
-              const moviePlaceholder =
-                document.getElementById("movie__placeholder");
-              const iframe = document.createElement("iframe");
-              iframe.src = `https://www.youtube.com/embed/${movieUrl?.results[0]?.key}`;
-              iframe.setAttribute("id", `movie${i}`);
-              if (i > 0) {
-                iframe.classList.add("component__hide");
-              }
-              moviePlaceholder.appendChild(iframe);
+  fetchMovies(movieSection)
+    .then(function (movieResult) {
+      if (movieResult) {
+        for (let i = 0; i < movieResult?.results?.length; i++) {
+          //  Because there's a then the function doesn't work syncronous
+          //  Have to rework it for it to work one by one
+          fetchMovieVideo(movieResult?.results[i].id)
+            .then(function (movieUrl) {
+              if (movieUrl?.results?.length > 0) {
+                //Render the movie video
+                const moviePlaceholder =
+                  document.getElementById("movie__placeholder");
+                const iframe = document.createElement("iframe");
+                iframe.src = `https://www.youtube.com/embed/${movieUrl?.results[0]?.key}`;
+                iframe.setAttribute("id", `movie${i}`);
+                if (i > 0) {
+                  iframe.classList.add("component__hide");
+                }
+                moviePlaceholder.appendChild(iframe);
 
-              //Render the carousel that contains all videos
-              const carousel = document.getElementById("carousel");
-              const carouselDot = document.createElement("i");
-              if (i == 0) {
-                carouselDot.classList.add(
-                  "fa-solid",
-                  "fa-regular",
-                  "fa-circle"
+                //Render the carousel that contains all videos
+                const carousel = document.getElementById("carousel");
+                const carouselDot = document.createElement("i");
+                if (i === 0) {
+                  carouselDot.classList.add(
+                    "fa-solid",
+                    "fa-regular",
+                    "fa-circle"
+                  );
+                } else {
+                  carouselDot.classList.add("fa-regular", "fa-circle");
+                }
+                carouselDot.setAttribute("id", `dot${i}`);
+                carouselDot.setAttribute("onclick", `carouselMove(${i})`);
+                carousel.appendChild(carouselDot);
+
+                // Add the movie info to the DOM
+                const movieInfo = document.getElementById("movie__info");
+                const movieContainer = document.createElement("div");
+                movieContainer.setAttribute("id", `info${i}`);
+                movieContainer.classList.add("movie__info__container");
+                movieContainer.setAttribute(
+                  "movieId",
+                  `${movieResult?.results[i].id}`
                 );
-              } else {
-                carouselDot.classList.add("fa-regular", "fa-circle");
+                movieContainer.setAttribute("counterId", `${i}`);
+
+                const movieInfoHeader = document.createElement("div");
+
+                renderMovieNameAndDescription(movieResult, movieInfoHeader, i);
+
+                renderMovieRatingAndCount(
+                  movieResult,
+                  movieInfoHeader,
+                  movieInfo,
+                  i,
+                  movieContainer
+                );
+
+                const movieDescription = document.createElement("p");
+                movieDescription.innerHTML = movieResult?.results[i]?.overview;
+                movieDescription.classList.add("movie__info__description");
+                movieContainer.appendChild(movieDescription);
               }
-              carouselDot.setAttribute("id", `dot${i}`);
-              carouselDot.setAttribute("onclick", `carouselMove(${i})`);
-              carousel.appendChild(carouselDot);
+            })
+            .catch(function (error) {
+              console.log("ERROR FETCHING MOVIES", error);
+            })
+            .finally(function () {
+              hideOnFirstRender();
+            });
 
-              // Add the movie info to the DOM
-              const movieInfo = document.getElementById("movie__info");
-              const movieContainer = document.createElement("div");
-              movieContainer.setAttribute("id", `info${i}`);
-              movieContainer.classList.add("movie__info__container");
-              movieContainer.setAttribute(
-                "movieId",
-                `${movieResult?.results[i].id}`
-              );
+          fetchReview(movieResult?.results[i]).then(function (movieReview) {});
+        }
 
-              const movieInfoHeader = document.createElement("div");
-
-              renderMovieNameAndDescription(movieResult, movieInfoHeader, i);
-
-              renderMovieRatingAndCount(
-                movieResult,
-                movieInfoHeader,
-                movieInfo,
-                i,
-                movieContainer
-              );
-
-              const movieDescription = document.createElement("p");
-              movieDescription.innerHTML = movieResult?.results[i]?.overview;
-              movieDescription.classList.add("movie__info__description");
-              movieContainer.appendChild(movieDescription);
-
-              // renderMovieCast(movieResult, movieInfo, "Diretor", i);
-            }
-          })
-          .catch(function (error) {
-            console.log("ERROR FETCHING MOVIES", error);
-          })
-          .finally(function () {
-            hideOnFirstRender();
-          });
-
-        fetchReview(movieResult?.results[i]).then(function (movieReview) {});
-        /*
-        const li = document.createElement("li");
-        const image = document.createElement("img");
-        const text = document.createElement("p");
-        text.innerText = movieResult?.results[i]?.original_title;
-        text.innerText += ` ${new Date(
-          movieResult?.results[i]?.release_date
-        ).getFullYear()}`;
-        image.src = `https://image.tmdb.org/t/p/original/${movieResult?.results[i]?.poster_path}`;
-
-          for (let x = 0; x < movieCast.cast.length; x++) {
-            if (movieCast?.cast[x]?.known_for_department === "director") {
-              console.log("AQUI", movieCast?.cast[x]);
-              const movieCastingDirectorName = document.createElement("span");
-              movieCastingDirectorName.innerHTML = movieCast?.cast[x]?.name;
-              movieCasting.append(movieCastingDirectorName);
-            }
-          }
-        */
+        document
+          .getElementById("page__loader")
+          .classList.add("component__hide");
+        document
+          .getElementById("page__loaded")
+          .classList.remove("component__hide");
       }
-
-      document.getElementById("page__loader").classList.add("component__hide");
-      document
-        .getElementById("page__loaded")
-        .classList.remove("component__hide");
-    }
-  });
+    })
+    .catch((error) => {
+      console.log("ERROR", error);
+    })
+    .finally(() => {});
 }
 
-const castRendered = {};
-
 $(window).scroll(function () {
-  if ($(window).scrollTop() + $(window).height() > $(document).height() - 300) {
-    const movieId = document.querySelector(".component__show__movie");
-    movieIdNumber = movieId.getAttribute("movieId");
-
-    if (castRendered[movieIdNumber]) {
-      return;
-    }
-    castRendered[movieIdNumber] = true;
-
-    console.log(movieIdNumber);
-    console.log(castRendered);
-
-    const castContainer = document.createElement("div");
-    const castContainerScreenwriter = document.createElement("div");
-    const castContainerActors = document.createElement("div");
-    castContainer.classList.add("movie__info__casting");
-    castContainerScreenwriter.classList.add("movie__info__casting");
-    castContainerActors.classList.add("movie__info__actors");
-
-    fetchCast(movieIdNumber).then((cast) => {
-      const castContainerRole = document.createElement("h3");
-      castContainerRole.innerHTML = "Diretor";
-      castContainer.append(castContainerRole);
-
-      const castContainerRoleScreenplay = document.createElement("h3");
-      castContainerRoleScreenplay.innerHTML = "Roteirista";
-      castContainerScreenwriter.append(castContainerRoleScreenplay);
-
-      const castContainerRoleActors = document.createElement("h3");
-      castContainerRoleActors.innerHTML = "Elenco";
-      castContainerActors.append(castContainerRoleActors);
-
-      const castContainerRoleValue = document.createElement("span");
-      const castContainerRoleValueScreenplay = document.createElement("span");
-
-      const containerActors = document.createElement("div");
-
-      for (let x = 0; x < cast.crew.length; x++) {
-        if (cast.crew[x].job === "Director") {
-          console.log("cast", cast.crew[x].name);
-          castContainerRoleValue.innerHTML = cast.crew[x].name;
-        }
-        if (cast.crew[x].job === "Screenplay") {
-          castContainerRoleValueScreenplay.innerHTML = cast.crew[x].name;
-        }
-      }
-
-      for (let x = 0; x < cast.cast.length; x++) {
-        if (cast.cast[x].known_for_department === "Acting") {
-          const actorName = document.createElement("span");
-          actorName.innerHTML = cast.cast[x].name;
-          containerActors.append(actorName);
-        }
-      }
-
-      castContainerActors.append(containerActors);
-      castContainer.append(castContainerRoleValue);
-      castContainerScreenwriter.append(castContainerRoleValueScreenplay);
-    });
-
-    movieId.append(castContainer);
-    movieId.append(castContainerScreenwriter);
-    movieId.append(castContainerActors);
+  if ($(window).scrollTop() + $(window).height() > $(document).height() / 2) {
+    renderFirstCast();
+    renderReviews();
   }
 });
